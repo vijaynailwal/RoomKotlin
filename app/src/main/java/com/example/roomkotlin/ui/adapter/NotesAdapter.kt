a320143cb4b807a17900a7e2a51911bddc832d54
vijaynailwal1992 <vijaynailwal92@gmail.com>
2020-01-11 15:49:01 +0530

    1. nav graph 2. room 3. co routines

---
+++ app/src/main/java/com/example/roomkotlin/ui/adapter/NotesAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.roomkotlin.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomkotlin.R
+import com.example.roomkotlin.ui.db.Note
+import com.example.roomkotlin.ui.ui.HomeFragmentDirections
+import kotlinx.android.synthetic.main.note_layout.view.*
+
+class NotesAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.note_layout, parent, false)
+        )
+    }
+
+    override fun getItemCount() = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.view.text_view_title.text = notes[position].title
+        holder.view.text_view_note.text = notes[position].note
+
+        holder.view.setOnClickListener {
+            val action =
+                HomeFragmentDirections.actionAddNote()
+            action.note = notes[position]
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    class NoteViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+}
